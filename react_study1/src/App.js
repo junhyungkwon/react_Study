282c5ab6c20cdec234a6c05c7a7b27ff63142ebf
junhyungkwon <aofry@naver.com>
2022-07-13 17:20:47 +0900

    event.preventDefault()를 사용하여 기본동작을 비활성화 시키기

--- react_study1/src/App.js
+++ react_study1/src/App.js
@@ -4,14 +4,23 @@ import './App.css';
 function Header(props){
   console.log('props',props,props.title)
   return <header>
-    <h1> <a href='/'>{props.title}</a></h1>
+    <h1> <a  href='/' onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode();
+
+    }}>{props.title}</a></h1>
   </header>
 }
 function Nav (props) { 
   const lis = []
   for(let i=0; i<props.topics.length; i++){
     let t= props.topics[i];
-    lis.push(<li key = {t.id}><a href={"/read/"+t.id}>{t.title}</a></li>)
+    lis.push(<li key = {t.id}>
+      <a id={t.id} href={"/read/"+t.id} onClick={(event)=>{
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a>
+    </li>)
   }
 
 return <nav>
@@ -37,8 +46,12 @@ function App() {
   ]
   return (
     <div>
-      <Header title= 'REACT'/>
-      < Nav topics={topics}></Nav>
+      <Header title= 'REACT' onChangeMode={()=>{
+        alert('제목입니다');
+      }}/>
+      < Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title = 'Welcome' body = 'Hello, web'/>
       
       
