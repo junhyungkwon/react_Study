580eed8fabb0877f972762ba51092cf8e32cdb19
junhyungkwon <aofry@naver.com>
2022-07-15 17:57:20 +0900

    Create Study 내가 원하는 데이터 만들어 화면에 노출

--- react_study1/src/App.js
+++ react_study1/src/App.js
@@ -39,14 +39,32 @@ function Article (props) {
 </article>
 
 }
+function Create (props){
+  return <article>
+    <h2>Create</h2>
+    <form  onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type = 'text' name = 'title' placeholder='title'/></p>
+      <p><textarea name = 'body' placeholder = 'body'></textarea></p>
+      <p><input type='submit' value='create'></input></p>
+      
+    </form>
+  </article>
+
+}
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setnextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...' },
     {id:2, title:'css', body:'css is ...' },
     {id:3, title:'javascript', body:'javascript is ...' }
-  ]
+  ]);
   let contend = null;
   if(mode=== 'WELCOME'){
     contend = <Article title = 'Welcome' body = 'Hello, web'/>
@@ -61,6 +79,15 @@ function App() {
       }
     }
     contend = <Article title = {title} body = {body}/>
+  } else if(mode==='CREATE'){
+    contend = <Create onCreate={(_title,_body)=>{
+      const newTopic= {id:nextId, title:_title, body:_body}
+      const newTopics =[...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+
+
+    }}> </Create>
   }
   return (
     <div>
@@ -72,6 +99,11 @@ function App() {
         setId(_id);
       }}></Nav>
       {contend}
+
+      <a href='/create' onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
       
       
     </div>
